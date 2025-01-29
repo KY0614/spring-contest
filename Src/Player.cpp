c671e71b786cdfec235f81a7b76a7a6b2f1c7ce8
MSI\raide <airen1dog@softbank.ne.jp>
2025-01-29 12:27:02 +0900

    クラスの追加

---
+++ Src/Player.cpp
@@ -0,0 +1,27 @@
+
+
+
+#include "Player.h"
+#include <DxLib.h>
+
+
+void Player::Init(void)
+{
+
+}
+void Player::Updeta(void)
+{
+
+}
+void Player::MoveUpdeta(void)
+{
+
+}
+void Draw(void)
+{
+
+}
+void Rerease(void)
+{
+
+}
---
+++ Src/Player.h
@@ -0,0 +1,22 @@
+#pragma once
+class Player
+{
+
+public:
+	//‰Šú‰»
+	void Init(void);
+	//ƒAƒvƒf
+	void Updeta(void);
+	//ˆÚ“®
+	void MoveUpdeta(void);
+	//•`‰æ
+	void Draw(void);
+	//íœ
+	void Rerease(void);
+
+private:
+
+
+
+};
+
