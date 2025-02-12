43c834eec040b5be81a80a6b521f14f169b43683
MSI\raide <airen1dog@softbank.ne.jp>
2025-02-12 14:23:02 +0900

    操作追加

--- Src/Object/Player.cpp
+++ Src/Object/Player.cpp
@@ -7,31 +7,70 @@
 
 void Player::Init(void)
 {
-	posX_ = 0;
-	posY_ = 0;
+	boxPosX_ = 0;
+	boxPosY_ = 0;
+	mousePosX_ = 0;
+	mousePosY_ = 0;
 	count_ = 0;
 	cr_ = 0x000000;
 }
 void Player::Updeta(void)
 {
-	MoveUpdeta();
+	MoveMouseUpdeta();
+	MoveKeyUpdata();
+	InputUpdeta();
 }
-void Player::MoveUpdeta(void)
+void Player::MoveMouseUpdeta(void)
 {
 	//マウス座標取得用
-	int mousPosX_, mousPosY_;
+	int mousPosX, mousPosY;
 	//マウス座標取得
-	GetMousePoint(&mousPosX_, &mousPosY_);
+	GetMousePoint(&mousPosX, &mousPosY);
 	//座標保存
-	posX_ = mousPosX_;
-	posY_ = mousPosY_;
-
-	//左クリック
+	mousePosX_ = mousPosX;
+	mousePosY_ = mousPosY;
+	
+}
+void Player::MoveKeyUpdata(void)
+{
 	InputManager& ins = InputManager::GetInstance();
+	//上移動
+	if (ins.IsTrgDown(KEY_INPUT_W))
+	{
+		boxPosY_ -= MOVE_Y;
+	}
+	//下移動
+	if (ins.IsTrgDown(KEY_INPUT_S))
+	{
+		boxPosY_ += MOVE_Y;
+	}
+	//右移動
+	if (ins.IsTrgDown(KEY_INPUT_D))
+	{
+		boxPosX_ += MOVE_X;
+	}
+	//左移動
+	if (ins.IsTrgDown(KEY_INPUT_A))
+	{
+		boxPosX_ -= MOVE_X;
+	}
+}
+
+void Player::InputUpdeta(void)
+{
+	InputManager& ins = InputManager::GetInstance();
+	//左クリック
 	if (ins.IsTrgMouseLeft())
 	{
-		count_+=1;
+		count_ += 1;
 	}
+
+	//右クリック
+	if (ins.IsTrgMouseRight())
+	{
+		count_ += 1;
+	}
+	//デバッグ
 	if ((count_ % 2) == 0)
 	{
 		cr_ = 0xffffff;
@@ -40,7 +79,6 @@ void Player::MoveUpdeta(void)
 	{
 		cr_ = 0x000000;
 	}
-
 }
 
 void Player::Draw(void)
@@ -50,10 +88,11 @@ void Player::Draw(void)
 
 
 	//デバッグ
-	DrawFormatString(0, 20, 0xffffff, "X%d", posX_);
-	DrawFormatString(0, 40, 0xffffff, "Y%d", posY_);
+	DrawFormatString(0, 20, 0xffffff, "X%d", mousePosX_);
+	DrawFormatString(0, 40, 0xffffff, "Y%d", mousePosY_);
 	DrawFormatString(0, 60, 0xffffff, "count%d", count_);
 	DrawBox(100, 100, 200, 200, cr_, true);
+	DrawBox(boxPosX_, boxPosY_, boxPosX_ + SIZE_X, boxPosY_ + SIZE_Y, 0x0000ff, true);
 }
 void Player::Rerease(void)
 {
--- Src/Object/Player.h
+++ Src/Object/Player.h
@@ -4,19 +4,27 @@ class Player
 
 public:
 	//定数
+	
+	//サイズ
+	static constexpr int SIZE_X = 32;
+	static constexpr int SIZE_Y = 32;
 
+	//移動量
+	static constexpr int MOVE_X = 32;
+	static constexpr int MOVE_Y = 32;
 
 
 private:
 	//変数
 	
+	//
+	int boxPosX_;
+	int boxPosY_;
 
-	//座標保存用
-	int posX_;
-	int posY_;
-
-
-
+	//マウス座標保存用
+	int mousePosX_;
+	int mousePosY_;
+	
 	//デバッグ
 	int count_;
 	int cr_;
@@ -28,8 +36,12 @@ public:
 	void Init(void);
 	//アプデ
 	void Updeta(void);
-	//移動
-	void MoveUpdeta(void);
+	//マウス移動
+	void MoveMouseUpdeta(void);
+	//キー移動
+	void MoveKeyUpdata(void);
+	//入力
+	void InputUpdeta(void);
 	//描画
 	void Draw(void);
 	//削除
